137e7f37c2fe6270128308868368c7bb0106816e
hope444-ctrl <Sanudhera@gmail.com>
2026-03-14 11:24:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -90,8 +90,7 @@ def chatbot_response(user_input, name):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+
