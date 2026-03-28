d580c9e63a708924e61faf0b320804000c1fbb35
hope444-ctrl <Sanudhera@gmail.com>
2026-03-28 23:08:40 +0530

    Update  script.js

--- static/ script.js
+++ static/ script.js
@@ -1,33 +1,34 @@
-function sendMessage() {
-
-   let userName = "";
+let userName = "";
 let askingName = true;  // true until user gives name
 
-    let input = document.getElementById("userInput").value;
+function sendMessage() {
+    let input = document.getElementById("userInput").value.trim();
     let messages = document.getElementById("messages");
-
     if (!input) return;
 
     // Display user's message
-   messages.innerHTML += "<p class='message user'>" + input + "</p>";
+    messages.innerHTML += "<p class='message user'>" + input + "</p>";
 
-    // Send to Flask
-    fetch("/chat", {
+    // Send to backend
+    fetch("/chat", { // works if HTML is served via Flask
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({ message: input })
     })
     .then(response => response.json())
     .then(data => {
-       messages.innerHTML += "<p class='message bot'>" + data.reply + "</p>";
+        messages.innerHTML += "<p class='message bot'>" + data.reply + "</p>";
         document.getElementById("userInput").value = "";
         messages.scrollTop = messages.scrollHeight;
 
-        // If bot just asked for name, mark it as provided
+        // Mark name as provided
         if (askingName) {
             userName = input;
             askingName = false;
         }
-  });
-
+    })
+    .catch(err => {
+        console.error(err);
+        messages.innerHTML += "<p class='message bot'>😅 Something went wrong!</p>";
+    });
 }
